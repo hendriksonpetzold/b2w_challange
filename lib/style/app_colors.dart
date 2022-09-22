c46d47b84f38c00f7b40c800d858efd453ce631a
Hendrikson Petzold <hendrikson_14@hotmail.com>
2022-09-22 07:42:34 -0300

    feat:app_colors

---
+++ lib/style/app_colors.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primaryColor = Color(0XFF5E2A84);
+  static const Color accentColor = Color(0XFFF15025);
+  static const Color textTitleColor = Color(0XFF2D3142);
+  static const Color testColor = Color(0XFF4A4A4A);
+  static const Color greyish = Color(0XFFAAAAAA);
+  static const Color greyishWhite = Color(0XFFD5D5D5);
+  static const Color backGroundColor = Color(0XFFFCFCFC);
+  static const Color white = Color(0XFFFCFCFC);
+}
